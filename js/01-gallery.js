992d78d773dc0881082772391a94850afa4fdb3b
Oleksandr Pavlishchev <velogitara@gmail.com>
2022-05-21 11:42:30 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,19 +1,12 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 console.log(galleryItems);
-// import { main } from '../node_modules/basiclightbox/src/scripts/main.js';
-// console.log(main);
-
 const galleryRef = document.querySelector('.gallery');
 const galleryMarkup = createGalleryMarkup(galleryItems);
 let lightBox = null;
 
 galleryRef.addEventListener('click', getLargeImgByClick);
 
-// document.body.insertAdjacentHTML(
-//   'beforeend',
-//   '<script src="https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js"></script>'
-// );
 console.log(document.body);
 function createGalleryMarkup(items) {
   return items
@@ -30,18 +23,14 @@ function createGalleryMarkup(items) {
     )
     .join('');
 }
-// console.log(galleryMarkup);
-
 galleryRef.innerHTML = galleryMarkup;
-
 function getLargeImgByClick(event) {
   event.preventDefault();
   if (!event.target.classList.contains('gallery__image')) {
     return;
   }
-
   const galleryEl = event.target;
-  // console.log(galleryEl.dataset.source);
+
   lightBox = basicLightbox.create(
     `
 		<img width="1400" height="900" src="${galleryEl.dataset.source}">
@@ -51,7 +40,6 @@ function getLargeImgByClick(event) {
 
   galleryRef.addEventListener('keydown', closeModalByEscapeBtn);
 }
-
 function closeModalByEscapeBtn(evt) {
   if (evt.code === 'Escape') {
     return lightBox.close();
